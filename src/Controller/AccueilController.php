b246073120e7b41e4719b3a4cafe73091e991411
Soden78 <demoineret.denis78@gmail.com>
2023-02-06 00:44:34 +0100

    start navbar Denis

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\VideoRepository;
+use App\Repository\CategorieRepository;
+use App\Repository\TypeVideoRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,12 +13,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AccueilController extends AbstractController
 {
   #[Route('/', name: 'accueil')]
-  public function accueil(VideoRepository $repoVideo)
+  public function accueil(VideoRepository $repoVideo, CategorieRepository $repoCategorie, TypeVideoRepository $repoTypeVideo)
   {
     $videos = $repoVideo->findAll();
+    $categories = $repoCategorie->findAll();
+    $typesVideos = $repoTypeVideo->findAll();
 
       return $this->render('accueil/index.html.twig', [
         'videos' => $videos,
+        'categories' => $categories,
+        'typesVideo' => $typesVideos,
         'controller_name' => 'AccueilController',
 
       ]);
