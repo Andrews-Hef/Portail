960230406d843fcc71cb03e487482513e5bbd798
Andrews-Hef <andrews.agricole@gmail.com>
2023-04-29 16:50:18 +0200

    [Andrews/Cinema]now can research for film

--- src/Controller/CinemaController.php
+++ src/Controller/CinemaController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\ResearchFilmType;
 use App\Service\CallApiService;
 use App\Repository\CategorieRepository;
 use App\Repository\TypeVideoRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CinemaController extends AbstractController
@@ -21,14 +25,32 @@ class CinemaController extends AbstractController
       }
  
     #[Route('/cinema', name: 'app_cinema')]
-    public function index(CallApiService $apiService): Response
+    public function index(CallApiService $apiService,Request $request): Response
     {   
         $categories = $this->categories;
         $typesVideos = $this->typesVideos;
+
+        //api request
         $results=$apiService->getNowPlaying();
         $allCinema=$apiService->getallCinema();
+        ///////////
+            //data for  form
+            $defaultData = ['message' => 'Type your message here'];
+            $form = $this->createFormBuilder($defaultData)
+            ->add('title', TextType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                // data is an array with "name", "email", and "message" keys
+                $data = $form->getData();
+                $data= $data['title'];
+                return $this->redirectToRoute("cinema.researchMovie",['data'=>$data]);
+            } 
+            
         return $this->render('cinema/index.html.twig', [
             'controller_name' => 'CinemaController',
+            'form'=>$form->createView(),
             'allCinema'=>$allCinema,
             'nowPlaying'=> $results,           
             'categories' => $categories,
@@ -75,11 +97,13 @@ class CinemaController extends AbstractController
         ]);
     }
     #[Route('/cinema/researchMovie', name: 'cinema.researchMovie')]
-    public function researchMovie(CallApiService $apiService,String $Name): Response
+    public function researchMovie(CallApiService $apiService,Request $request): Response
     {   
         $categories = $this->categories;
         $typesVideos = $this->typesVideos;
-
+        //retrieve data from form 
+        $Name=$request->query->get('data');
+        //$Name=$Name['title'];
         $results=$apiService->researchMovie($Name);
         return $this->render('cinema/researchMovie.html.twig', [
             'controller_name' => 'CinemaController',
