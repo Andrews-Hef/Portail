b6fb0b89dd8d9792ca82f6bd2acac8f02b198200
Soden78 <demoineret.denis78@gmail.com>
2023-05-23 22:10:33 +0200

    test prod + route

--- src/Form/BarreDeRechercheAutocomplete.php
+++ src/Form/BarreDeRechercheAutocomplete.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class VideoType extends AbstractType
+class BarreDeRechercheAutocomplete  extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
